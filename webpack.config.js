e5c3ff38eab402b54cc327ffca0895a87aff2b6e
dudulaopo833 <1294318974@qq.com>
2017-08-13 18:58:49 +0800

    Create the structure and setup the webpack config

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var path = require('path');
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: "./index.js",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js"	
+	},
+	devtool: "source-map",
+	context: __dirname,
+	module: {
+		rules:[
+			{
+				test: /\.js?$/,
+				include: [
+					path.resolve(__dirname, "actions"),
+					path.resolve(__dirname, "reducers"),
+					path.resolve(__dirname, "components"),
+					path.resolve(__dirname, "containers")
+				],
+				loader: "babel-loader",
+				options: {
+					presets: ["es2015"]
+				}
+			},
+			{
+				test: /\.html$/,
+				loader: "html-loader"
+			}
+		]
+	},
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		historyApiFallback: true,
+		hot: true,
+		noInfo: true
+	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: "index.html",
+			inject: 'head'
+		})
+	]	
+};
